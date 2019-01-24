82d5ab6e6f6c8b56ce46f24006d23da5934d81fc
nclaudia <claudinelega@gmail.com>
2019-01-24 21:30:01 +0200

    b

--- blit.js
+++ blit.js
@@ -1,46 +1,24 @@
-//Test browser support
-const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
+//this is a sample flashlight effect
 
-if (SUPPORTS_MEDIA_DEVICES) {
-  //Get the environment camera (usually the second one)
-  navigator.mediaDevices.enumerateDevices().then(devices => {
-  
-    const cameras = devices.filter((device) => device.kind === 'videoinput');
-
-    if (cameras.length === 0) {
-      throw 'No camera found on this device.';
+$(document).ready(function() {
+  $(this).mousemove(function(e) {
+    $("#light").css({
+      "top": e.pageY - 250,
+      "left": e.pageX - 250
+    })
+  }).mousedown(function(e) {
+    switch (e.which) {
+        case 1:
+            $("#light").toggleClass("light-off");
+            break;
+        case 2:
+            console.log('Middle Mouse button pressed.');
+            break;
+        case 3:
+            console.log('Right Mouse button pressed.');
+            break;
+        default:
+            console.log('You have a strange Mouse!');
     }
-    const camera = cameras[cameras.length - 1];
-
-    // Create stream and get video track
-    navigator.mediaDevices.getUserMedia({
-      video: {
-        deviceId: camera.deviceId,
-        facingMode: ['user', 'environment'],
-        height: {ideal: 1080},
-        width: {ideal: 1920}
-      }
-    }).then(stream => {
-      const track = stream.getVideoTracks()[0];
-
-      //Create image capture object and get camera capabilities
-      const imageCapture = new ImageCapture(track)
-      const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
-
-        //todo: check if camera has a torch
-
-        //let there be light!
-        const btn = document.querySelector('.switch');
-        btn.addEventListener('click', function(){
-          track.applyConstraints({
-            advanced: [{torch: true}]
-          });
-        });
-      });
-    });
-  });
-  
-  //The light will be on as long the track exists
-  
-  
-}
+  })
+})
