e46590c2ede734aac92a6d944a8b6c8d65bb9c7e
nclaudia <44379357+nclaudia@users.noreply.github.com>
2019-01-25 09:56:18 +0200

    Delete microfon1.js

--- microfon1.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("id_speak1").addEventListener("click",on_speak);
-var synth=window.speechSynthesis;
-function on_speak(){
-	var enunt= new SpeechSynthesisUtterance();
-	enunt.lang="en-US";
-	enunt.text=document.getElementById("id_text1").value;
-	synth.speak(enunt);
-}
